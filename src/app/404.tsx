66cafc2c60d5cc740e17a78bcaf3ed71ef99608b
Vincent Pruvost <vincent@MacBook-Air-de-Vincent.local>
2024-10-05 12:49:26 +0200

    Votre message de commit ici

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -5,7 +5,7 @@ const Custom404 = () => {
   return (
     <div>
       <h1>404 - Page non trouvée</h1>
-      <p>Il s&apos;agit d&apos;une page qui n&apos;existe pas.</p>
+      <p>Cette page n'existe pas.</p>
       <Link href="/">Retour à la page principale</Link>
     </div>
   );
