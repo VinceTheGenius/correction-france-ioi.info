6e06780a665fc15cd5aa7259a64c1ecab34bd164
Vincent Pruvost <vincent@Mac.lan>
2024-10-05 22:13:17 +0200

    Fix du mode sombre qui entranait un affichage peu lisible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { useState, useEffect } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { tomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
-// Définissez une interface pour le contenu
+// Interface pour le contenu
 interface Arborescence {
-  [key: string]: Record<string, string> | null; // Vous pouvez ajuster le type selon la structure de vos données
+  [key: string]: Record<string, string> | null;
 }
 
 const Explorer = () => {
@@ -18,7 +18,7 @@ const Explorer = () => {
   useEffect(() => {
     const fetchArborescence = async () => {
       const response = await fetch("/api/niveau1");
-      const data: Arborescence = await response.json(); // Assurez-vous que les données correspondent à l'interface définie
+      const data: Arborescence = await response.json();
       setArborescence(data);
     };
 
@@ -42,47 +42,47 @@ const Explorer = () => {
     }));
   };
 
-  const renderArborescence = (arborescence: Arborescence) => {
-    return Object.entries(arborescence).map(([nom, contenu]) => (
-      <div key={nom} className="mb-2">
-        <h3
-          className="font-semibold flex items-center cursor-pointer"
-          onClick={() => {
-            if (typeof contenu === 'object' && contenu !== null) {
-              toggleDossier(nom);
-            }
-          }}
-        >
-          <img
-            src={
-              dossiersOuverts[nom]
-                ? "/icons/folder-open.png"
-                : "/icons/folder-close.png"
-            }
-            alt={dossiersOuverts[nom] ? "Dossier ouvert" : "Dossier fermé"}
-            className="mr-2 w-5 h-5"
-          />
-          {nom}
-        </h3>
-        {dossiersOuverts[nom] && contenu && (
-          <ul className="list-disc pl-5">
-            {Object.keys(contenu).map((item) => (
-              <li
-                key={item}
-                onClick={() => handleClickFichier(`${nom}/${item}`)}
-                className="cursor-pointer hover:bg-gray-200 flex items-center py-1"
-              >
-                <img
-                  src="/icons/file-python.png"
-                  alt="Fichier Python"
-                  className="mr-2 w-5 h-5"
-                />
-                {item}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+  const renderArborescence = (arbo: Arborescence) => {
+    return Object.entries(arbo).map(([nom, contenu]) => (
+        <div key={nom} className="mb-2">
+          <h3
+              className="font-semibold flex items-center cursor-pointer"
+              onClick={() => {
+                if (typeof contenu === 'object' && contenu !== null) {
+                  toggleDossier(nom);
+                }
+              }}
+          >
+            <img
+                src={
+                  dossiersOuverts[nom]
+                      ? "/icons/folder-open.png"
+                      : "/icons/folder-close.png"
+                }
+                alt={dossiersOuverts[nom] ? "Dossier ouvert" : "Dossier fermé"}
+                className="mr-2 w-5 h-5"
+            />
+            {nom}
+          </h3>
+          {dossiersOuverts[nom] && contenu && (
+              <ul className="list-disc pl-5">
+                {Object.keys(contenu).map((item) => (
+                    <li
+                        key={item}
+                        onClick={() => handleClickFichier(`${nom}/${item}`)}
+                        className="cursor-pointer py-1 flex items-center transition-colors duration-300 hover:bg-gray-200 dark:hover:bg-contentBg dark:hover:text-white"
+                    >
+                      <img
+                          src="/icons/file-python.png"
+                          alt="Fichier Python"
+                          className="mr-2 w-5 h-5"
+                      />
+                      {item}
+                    </li>
+                ))}
+              </ul>
+          )}
+        </div>
     ));
   };
 
@@ -95,47 +95,49 @@ const Explorer = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen">
-      <div className="bg-yellow-300 text-center py-2 font-bold">
-        Corrigés du Niveau 1 de France IOI (niveau obligatoire)  
-        <div className="flex items-center justify-center mt-2">
-          <img
-            src="/icons/warning.png"
-            alt="Avertissement"
-            className="mr-2 w-4 h-4"
-          />
-          <span className="opacity-75">
-            Nb : il est très fortement conseillé de faire les exercices soi-même. Ces corrigés sont uniquement là pour vous débloquer sur certains exercices.
+      <div className="flex flex-col h-screen">
+        <div className="bg-yellow-300 text-center py-2 font-bold text-black">
+          Corrigés du Niveau 1 de France IOI (niveau obligatoire)
+          <div className="flex items-center justify-center mt-2">
+            <img
+                src="/icons/warning.png"
+                alt="Avertissement"
+                className="mr-2 w-4 h-4"
+            />
+            <span className="opacity-70 text-black">
+            Nb : il est très fortement conseillé de faire les exercices
+            soi-même. Ces corrigés sont uniquement là pour vous débloquer sur
+            certains exercices.
           </span>
-        </div>
-      </div>
-      <div className="flex flex-col md:flex-row flex-grow">
-        <div className="w-full md:w-1/3 p-4 bg-gray-100 rounded-lg shadow-lg overflow-y-auto">
-          <h2 className="text-lg font-bold mb-4">Arborescence</h2>
-          {renderArborescence(arborescence)}
-        </div>
-        <div className="w-full md:w-2/3 p-4 bg-white rounded-lg shadow-lg overflow-y-auto">
-          <div className="flex justify-between items-center mb-2">
-            <h3 className="font-bold">Contenu du fichier</h3>
-            <button
-              onClick={copyCodeToClipboard}
-              className="text-xs px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
-            >
-              {copied ? "Copié !" : "Copier le code"}
-            </button>
           </div>
-          {contenuFichier ? (
-            <SyntaxHighlighter language="python" style={tomorrow}>
-              {contenuFichier}
-            </SyntaxHighlighter>
-          ) : (
-            <div className="text-center text-gray-500">
-              Sélectionnez un fichier pour voir le contenu.
+        </div>
+        <div className="flex flex-col md:flex-row flex-grow">
+          <div className="w-full md:w-1/3 p-4 bg-sidebarBg rounded-lg shadow-lg overflow-y-auto">
+            <h2 className="text-lg font-bold mb-4">Arborescence</h2>
+            {renderArborescence(arborescence)}
+          </div>
+          <div className="w-full md:w-2/3 p-4 bg-contentBg rounded-lg shadow-lg overflow-y-auto">
+            <div className="flex justify-between items-center mb-2">
+              <h3 className="font-bold">Contenu du fichier</h3>
+              <button
+                  onClick={copyCodeToClipboard}
+                  className="text-xs px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+              >
+                {copied ? "Copié !" : "Copier le code"}
+              </button>
             </div>
-          )}
+            {contenuFichier ? (
+                <SyntaxHighlighter language="python" style={tomorrow}>
+                  {contenuFichier}
+                </SyntaxHighlighter>
+            ) : (
+                <div className="text-center text-gray-500">
+                  Sélectionnez un fichier pour voir le contenu.
+                </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--background)", // Fond principal
+        foreground: "var(--foreground)", // Texte principal
+        sidebarBg: "var(--sidebar-bg)", // Fond de la sidebar
+        contentBg: "var(--content-bg)", // Fond de la partie droite
       },
     },
   },
