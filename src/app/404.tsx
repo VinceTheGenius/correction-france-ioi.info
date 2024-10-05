be0ed369bb74aeaa36e35412fde8347092af4186
Vincent Pruvost <vincent@MacBook-Air-de-Vincent.local>
2024-10-05 12:56:15 +0200

    Page 404 - apostrophe

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -5,7 +5,7 @@ const Custom404 = () => {
   return (
     <div>
       <h1>404 - Page non trouvée</h1>
-      <p>Cette page n'existe pas.</p>
+      <p>Cette page n&apos;existe pas.</p>
       <Link href="/">Retour à la page principale</Link>
     </div>
   );
